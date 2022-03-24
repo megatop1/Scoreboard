3f66cf7f0798d9521f19b7b7949a607f89d2cb56
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-03-23 22:22:37 -0400

    Update install-nagios-ubuntu.sh

--- install-nagios-ubuntu.sh
+++ install-nagios-ubuntu.sh
@@ -3,7 +3,7 @@ sudo apt -y install nagios4 nagios-plugins-contrib nagios-nrpe-plugin
 sudo a2enmod authz_groupfile auth_digest
 
 mv /etc/apache2/conf-enabled/nagios4-cgi.conf /etc/apache2/conf-enabled/nagios4-cgi.conf.temp
-cp nagios4-cgi.conf.correct /etc/apache2/conf-enabled/nagios4-cgi.conf
+cp nagios/nagios4-cgi.conf.correct /etc/apache2/conf-enabled/nagios4-cgi.conf
 
 sudo systemctl restart apache2
 
